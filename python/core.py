b6526c3a8f1516dac3733d03940c2306ced12a84
dmbramson <dmbramson@gmail.com>
2022-08-12 21:16:16 -0500

    Add adaptive gradient boosting

--- python/core.py
+++ python/core.py
@@ -58,11 +58,35 @@ def grad_descent(
 ) -> npt.NDArray[np.number]:
     """Optimize logistic regression parameters using gradient descent."""
     i = 1
-    while np.linalg.norm(b) > threshold:
-        grad = calc_logloss_grad(X, y, b, eps)
+    grad = calc_logloss_grad(X, y, b, eps)
+    while np.linalg.norm(grad) > threshold:
         step = grad * alpha
         b = b - step
         print("Iteration {}: b = {}, grad.norm = {}".format(i, b, np.linalg.norm(grad)))
+        grad = calc_logloss_grad(X, y, b, eps)
+        i = i + 1
+        if i > max_iter:
+            break
+    return b
+
+
+def adagrad(
+    X: npt.NDArray[np.number],
+    y: npt.NDArray[np.number],
+    b: npt.NDArray,
+    eps: np.number = EPS,
+    threshold: float = 1e-2,
+    alpha: float = ALPHA,
+    max_iter: int = 10000,
+) -> npt.NDArray[np.number]:
+    """Optimize logistic regression parameters using adaptive gradient descent."""
+    i = 1
+    grad = calc_logloss_grad(X, y, b, eps)
+    while np.linalg.norm(grad) > threshold:
+        step = grad * alpha/(np.linalg.norm(grad)+1e-8)
+        b = b - step
+        print("Iteration {}: b = {}, grad.norm = {}".format(i, b, np.linalg.norm(grad)))
+        grad = calc_logloss_grad(X, y, b, eps)
         i = i + 1
         if i > max_iter:
             break
@@ -86,23 +110,19 @@ y = test_data["class"]
 b = np.array([1, 1, 1])
 yhat = logistic(X, b)
 z = logloss(y, yhat)
-grad = calc_logloss_grad(X, y, b)
-step = grad * ALPHA
-b = b - step
 
 # Fails to converge in 10k, final gradient norm is 0.0275
 # Still finds good coefficients that actually result in 100% accuracy
-b = np.array([1, 1, 1])
-i = 1
-while np.linalg.norm(b) > 1e-2:
-    grad = calc_logloss_grad(X, y, b)
-    step = grad * ALPHA
-    b = b - step
-    print("Iteration {}: b = {}, grad.norm = {}".format(i, b, np.linalg.norm(grad)))
-    i = i + 1
-    if i > 10000:
-        break
-
-yhat = logistic(X, b)
+b2 = grad_descent(X, y, b)
+yhat = logistic(X, b2)
 z = logloss(y, yhat)  # 0.083
 ypred = np.round(yhat, 0)
+# Converges in 2152 iterations
+# Obviously also has 100% accuracy
+b3 = adagrad(X, y, b)
+yhat = logistic(X, b3)
+z = logloss(y, yhat)  # 0.0314
+ypred = np.round(yhat, 0)
+sum(y-ypred)
+
+
